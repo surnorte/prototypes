df5a71929189b40b9d7a0b70660f9d04d9d99378
Zachary Martin <zachmart@hotmail.com>
2015-03-24 23:21:17 -0500

    renamed some files in the parser prototype with grid

---
+++ parser_prototype_with_grid/js/ImportData.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zacharymartin on 3/24/15.
+ */
+
+function ImportData(){
+    // TODO - implement this data structure
+
+    //this.experimetIdentifier = -1;
+    //this.experimentLabels = {category -> label};
+    //this.plateLabels = {plateId -> {category -> label}};
+    //this.wellLabels = {plateId->{row->{column->{category->label}}}}
+}
